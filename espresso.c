3e5b755847a714ffbb3f88015fa7bf9bfa99476f
Mitchell Pleune <mitchpleune@gmail.com>
2018-02-19 00:39:55 -0500

    better reading, 404s, and CDSS
    + Use CDSS library for stack_t
    + Read buffer one chunk per loop.
     - Makes reading loop much simpler
    + Async reads
     - Check for EOF vs. incomplete
    + Send 404 by default.
     - Less confusing for browsers

--- espresso.c
+++ espresso.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <poll.h>
@@ -8,12 +9,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <cdss/minmax.h>
 #include <cdss/stack.h>
 
 #define MAX_REQUEST_LEN 20000000 // 20m
 #define FIRST_BUFFER_SIZE 1000 // 1k
+#define MAX_READ_BUF_CHUNK 10000 // 10k
 
-enum http_status { HTTP_WAIT_FOR_REQUEST_HEADER, HTTP_SHUTDOWN };
+enum http_status { HTTP_WAIT_FOR_REQUEST_HEADER, HTTP_CLOSE, HTTP_CLOSED };
 struct http_state {
     enum http_status   state;
     struct sockaddr_in client;
@@ -21,7 +24,10 @@ struct http_state {
     size_t             recv_buf_size;
     size_t             recv_buf_read_index;
     size_t             parse_index;
+    int                read_eof;
 };
+
+enum connection_state { CONN_ACTIVE = 0, CONN_CLOSED };
 
 int
 espresso_listen(int port)
@@ -57,66 +63,55 @@ init_http_state(struct http_state *state)
     state->recv_buf_size       = FIRST_BUFFER_SIZE;
     state->recv_buf_read_index = 0;
     state->parse_index         = 0;
+    state->read_eof            = 0;
 }
 
 static void
 connection_continue_reading(struct http_state *state, int fd)
 {
-    int again = 0;
-    do
+    if (state->recv_buf_size >= MAX_REQUEST_LEN || state->read_eof)
     {
-        ssize_t avail = state->recv_buf_size - state->recv_buf_read_index;
-        ssize_t num_read;
+        state->read_eof = 1;
+        return;
+    }
 
-        if ((num_read = read(fd, state->recv_buf + state->recv_buf_read_index, avail)) == avail)
-        {
-            size_t new_size = state->recv_buf_size * 2;
+    ssize_t avail    = state->recv_buf_size - state->recv_buf_read_index;
+    ssize_t num_read = read(fd, state->recv_buf + state->recv_buf_read_index, avail);
+    state->recv_buf_read_index += num_read;
 
-            if (new_size >= MAX_REQUEST_LEN)
-            {
-                state->state = HTTP_SHUTDOWN;
-                break;
-            }
+    size_t new_size = state->recv_buf_size + MIN(MAX_READ_BUF_CHUNK, state->recv_buf_size);
+    char * new_buf  = realloc(state->recv_buf, new_size);
+    if (new_buf)
+    {
+        state->recv_buf      = new_buf;
+        state->recv_buf_size = new_size;
+    }
 
-            printf("doingd it %li \t %li\n", new_size, state->recv_buf_size);
-            fflush(stdout);
-            char *new_buf = realloc(state->recv_buf, new_size);
-            printf("did it \n");
-            fflush(stdout);
-
-            if (new_buf)
-            {
-                again                = 1;
-                state->recv_buf      = new_buf;
-                state->recv_buf_size = new_size;
-            }
-        }
-        else
-        {
-            again = 0;
-            state->recv_buf_read_index += num_read;
-        }
-    } while (again);
+    if (num_read == 0 && !(errno == EAGAIN || errno == EWOULDBLOCK)) state->read_eof = 1;
 }
 
-void
-espresso_handle_connection(struct http_state *http_state, struct pollfd *fd)
+enum connection_state
+espresso_handle_connection(struct http_state *state, struct pollfd *fd)
 {
-    if (fd->revents & POLLIN) { connection_continue_reading(http_state, fd->fd); }
+    enum connection_state ret = CONN_ACTIVE;
 
-    switch (http_state->state)
+    if (fd->revents & POLLIN) { connection_continue_reading(state, fd->fd); }
+
+    switch (state->state)
     {
     case HTTP_WAIT_FOR_REQUEST_HEADER:
-        while (http_state->parse_index < http_state->recv_buf_read_index)
-            printf("%c", http_state->recv_buf[http_state->parse_index++]);
-        break;
-    case HTTP_SHUTDOWN:
-        if (http_state->recv_buf) free(http_state->recv_buf);
-        shutdown(fd->fd, 2);
-        break;
+        while (state->parse_index < state->recv_buf_read_index) printf("%c", state->recv_buf[state->parse_index++]);
+        if (state->read_eof) break;
+    case HTTP_CLOSE:
+        free(state->recv_buf);
+        write(fd->fd, "HTTP/1.1 404 Not Found\r\n\r\n", 26);
+        close(fd->fd);
+        state->state = HTTP_CLOSED;
+    case HTTP_CLOSED: ret = CONN_CLOSED; break;
     }
 
     fd->revents = 0; // reset for next exent
+    return ret;
 }
 
 int
@@ -131,9 +126,10 @@ main()
     connections_poll = stack_create(sizeof(struct pollfd), 512, 2.0);
 
     /*
-     * Main loop: (2 parts)
+     * Main loop:
      *   1. Accept any new connections
-     *   2. use poll() to check for new input
+     *   2. Use poll() to check for new input
+     *   3. Remove closed connections
      */
     while (1)
     {
@@ -142,6 +138,9 @@ main()
         struct pollfd     new_client_poll;
         while ((new_client_poll.fd = accept(socket, (struct sockaddr *) &new_client_http.client, &clen)) > 0)
         {
+
+            fcntl(new_client_poll.fd, F_SETFL, O_NONBLOCK);
+
             new_client_poll.events  = POLLIN;
             new_client_poll.revents = 0;
 
@@ -162,7 +161,12 @@ main()
         poll(stack_element_ref(connections_poll, 0), num_connections, 10); // 10ms
 
         for (int connection = 0; connection < num_connections; connection++)
-            if (connections_poll_data[connection].fd >= 1)
-                espresso_handle_connection(&connections_http_data[connection], &connections_poll_data[connection]);
+            if (connections_poll_data[connection].revents)
+                if (espresso_handle_connection(&connections_http_data[connection],
+                                               &connections_poll_data[connection]) == CONN_CLOSED)
+                {
+                    stack_element_replace_from_end(connections_http, connection);
+                    stack_element_replace_from_end(connections_poll, connection);
+                }
     }
 }
